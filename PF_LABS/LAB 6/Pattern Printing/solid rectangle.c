616112f2e1be8839f86021d70cb06852878ea0e5
alishanaushad008-del <alishanaushad008@gmail.com>
2025-10-05 01:09:26 +0500

    Add files via upload

---
+++ PF_LABS/LAB 6/Pattern Printing/solid rectangle.c
@@ -0,0 +1,15 @@
+ #include<stdio.h>
+ int main(){
+int n;
+printf("enter a number :");
+scanf("%d",&n);
+for(int i=1;i<=n;i++){
+for(int i=1;i<=n;i=i+1){
+    printf("*");
+}
+    printf("\n");
+
+}
+
+    return 0;
+ }
